8908587a6f9ce6c59d959dc0eaf593579c6bd741
W. Pereira Santos <w.pereira.santos@avanade.com>
2020-01-22 13:44:21 -0300

    Primeiro commit node-api

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const requireDir = require('require-dir')
+const app = express();
+
+app.use(express.json())
+
+
+//Conectando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
+requireDir('./src/models');
+
+//Rotas
+app.use('/api', require('./src/routes'))
+
+//Servidor rodando na porta 3003
+app.listen(3003, () => {
+    console.log('Rodando na porta 3003')
+})
